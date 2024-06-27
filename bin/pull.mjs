e641b892131c8f15890b95b241126fd810b0c0c2
Marek Kaput <marek.kaput@swmansion.com>
2024-06-27 17:44:24 +0200

    refactor pull.mjs and emit trailing newlines
    Signed-off-by: Marek Kaput <marek.kaput@swmansion.com>

--- bin/pull.mjs
+++ bin/pull.mjs
@@ -4,14 +4,34 @@ import {fileURLToPath} from "node:url";
 const syntaxPath = packagePath("syntaxes/cairo.tmLanguage.json");
 const packageJsonPath = packagePath("package.json");
 
-const syntax = await fetch("https://raw.githubusercontent.com/starkware-libs/cairo/main/vscode-cairo/syntaxes/cairo.tmLanguage.json").then(p => p.json())
-await fs.writeFile(syntaxPath, JSON.stringify(syntax, null, 2), "utf-8");
+const syntax = await fetchJson("https://raw.githubusercontent.com/starkware-libs/cairo/main/vscode-cairo/syntaxes/cairo.tmLanguage.json");
+await writeJson(syntaxPath, syntax);
 
-const {version} = await fetch("https://github.com/starkware-libs/cairo/raw/main/vscode-cairo/package.json").then(p => p.json());
-const origPackageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf-8"));
+const {version} = await fetchJson("https://github.com/starkware-libs/cairo/raw/main/vscode-cairo/package.json");
+const origPackageJson = await readJson(packageJsonPath);
 const packageJson = {...origPackageJson, version};
-await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), "utf-8");
+await writeJson(packageJsonPath, packageJson);
 
 function packagePath(path) {
   return fileURLToPath(new URL(`../${path}`, import.meta.url))
+}
+
+async function fetchJson(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+
+async function readJson(path) {
+  return JSON.parse(await fs.readFile(path, "utf-8"));
+}
+
+async function writeJson(path, obj) {
+  let str = JSON.stringify(obj, null, 2);
+
+  // Ensure trailing newline to match EditorConfig settings.
+  if (!str.endsWith("\n")) {
+    str += "\n";
+  }
+
+  return await fs.writeFile(path, str, "utf-8");
 }
